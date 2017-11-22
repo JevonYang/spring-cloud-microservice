38b95b4d59cc47d3dae32418c29c7793eec9a8fa
JevonYang <yangzijing08@gmail.com>
2017-11-22 16:27:08 +0800

    update controller

--- src/main/java/com/yang/demo/controller/Controller.java
+++ src/main/java/com/yang/demo/controller/Controller.java
@@ -3,6 +3,7 @@ package com.yang.demo.controller;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.google.gson.Gson;
 import com.yang.demo.entity.DepartmentEntity;
 import com.yang.demo.entity.RoleEntity;
 import com.yang.demo.entity.UserEntity;
@@ -36,34 +37,30 @@ public class Controller {
         return "hello, world";
     }
 
-    @RequestMapping(value = "/{name}", method = RequestMethod.GET, produces="application/json")
-    public UserEntity User (@PathVariable("name") String name) {
-        System.out.println("hello");
-
-        UserEntity user1 = new UserEntity();
-        user1.setName(name);
-
-        DepartmentEntity departmentEntity = new DepartmentEntity();
-        departmentEntity.setName("开发组");
-        departmentRepository.save(departmentEntity);
-
-        RoleEntity roleEntity = new RoleEntity();
-        roleEntity.setName("Admin");
-        roleRepository.save(roleEntity);
-
-        user1.setCreatedate(new Date());
-        user1.setDepartmentEntity(departmentEntity);
-        user1.setRoles(roleEntity);
-        userRepository.save(user1);
-
-        return user1;
-    }
-
-    @RequestMapping(value = "/list", method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping(value = "/list", method = RequestMethod.POST, produces = "application/json")
     public JSONArray userList(){
         List<UserEntity> users = userRepository.findAll();
         JSONArray userStr = (JSONArray) JSON.toJSON(users);
         return userStr;
     }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public UserEntity User(@PathVariable("id") Long id) {
+        UserEntity user = userRepository.getOne(id);
+        return user;
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST, consumes = "text/plain")
+    public String addUser(@RequestBody UserEntity user){
+
+        System.out.println(user);
+        user.setCreatedate(new Date());
+        System.out.println(user.getName());
+        System.out.println(user.getRoles().getName());
+        System.out.println(user.getDepartmentEntity().getName());
+        userRepository.save(user);
+        return "redirect:/list";
+    }
+
 
 }
