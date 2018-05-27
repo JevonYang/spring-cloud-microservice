7a0c922c767ec6a17ef6d32de4c3ea134911c113
JevonYang <yangzijing08@gmail.com>
2018-05-27 20:17:02 +0800

    add zuul module 加入zuul模块

---
+++ gateway/src/main/java/gateway/GatewayApplication.java
@@ -0,0 +1,14 @@
+package gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+
+
+@EnableZuulServer
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
